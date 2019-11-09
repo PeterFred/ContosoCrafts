6cba47180f9368ebb94e70be11a04adff8783776
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-10 09:43:45 +1300

    Data added to index.cshtml

--- ContosoCrafts/Pages/Index.cshtml.cs
+++ ContosoCrafts/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.Models;
+using ContosoCrafts.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,21 @@ namespace ContosoCrafts.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private JsonFileProductService _productService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
         {
-            _logger = logger;
+           _logger = logger;
+            _productService = productService;
+
         }
 
         public void OnGet()
         {
-
+            Products = _productService.GetProducts();
         }
     }
 }
